0fa12ca95b287d35fccf0d3e9bdb5d1477999f57
Dawit-Engdawork <dawitengdawork33@gmail.com>
2024-05-22 17:58:42 +0300

    changes

--- backend/src/controllers/MyUserController.ts
+++ backend/src/controllers/MyUserController.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from "express";
 import User from "../models/user";
-import { log } from "console";
 
 const createCurrentUser = async (req: Request, res: Response) => {
   // 1. check if the user exist
