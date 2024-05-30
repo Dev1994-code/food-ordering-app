1509829a1b7fe027c6e8bef7ff774a77da092165
Dawit-Engdawork <dawitengdawork33@gmail.com>
2024-05-30 17:47:41 +0300

    changes

---
+++ frontend/src/forms/user-profile-form/UserProfileForm.tsx
@@ -0,0 +1,43 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Form, FormDescription } from "@/components/ui/form";
+
+const formSchema = z.object({
+  email: z.string().optional(),
+  name: z.string().min(1, "name is required"),
+  addressLine1: z.string().min(1, "Address Line 1 is required"),
+  city: z.string().min(1, "City is required"),
+  country: z.string().min(1, "Country is required"),
+});
+
+type UserFormData = z.infer<typeof formSchema>;
+
+type Props = {
+  onSave: (userProfileData: UserFormData) => void;
+  isLoading: boolean;
+};
+
+const UserProfileForm = ({ onSave, isLoading }: Props) => {
+  const form = useForm<UserFormData>({
+    resolver: zodResolver(formSchema),
+  });
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSave)}
+        className="space-y-4 bg-gray-50 rounded-lg md:p-10"
+      >
+        <div>
+          <h2 className="text-2xl font-bold">User Profile Form</h2>
+        </div>
+        <FormDescription>
+          View and change your user profile information here
+        </FormDescription>
+      </form>
+    </Form>
+  );
+};
+
+export default UserProfileForm;
